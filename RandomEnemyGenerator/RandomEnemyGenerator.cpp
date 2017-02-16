dab2299aecffbfa9b5931254e5c4c8dc7ecfdc3d
joanreigbergua <joanreigbergua@enti.cat>
2017-02-16 10:55:44 +0100

    Hago el enum y el struct de los tipos de enemigos

---
+++ RandomEnemyGenerator/RandomEnemyGenerator.cpp
@@ -0,0 +1,21 @@
+// RandomEnemyGenerator.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+enum EnemyType{ Zombie,Vampire, Ghost, Witch};
+
+struct Enemy {
+	string name;
+	int health;
+};
+
+int main()
+{
+
+    return 0;
+}
+
